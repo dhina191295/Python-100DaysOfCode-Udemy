7c0f6a764464626bf2de6e4735ec00bf9b425e54
Dhinesh Kumar <77201544+dhina191295@users.noreply.github.com>
2022-04-22 12:28:22 +0530

    vs code try

--- GuessTheNumber/main.py
+++ GuessTheNumber/main.py
@@ -2,11 +2,13 @@
 
 # Include an ASCII art logo.
 # Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
+# Check user's guess against actual answer. Print "Too high." or "Too low." \
+# depending on the user's answer.
 # If they got the answer correct, show the actual answer to the player.
 # Track the number of turns remaining.
 # If they run out of turns, provide feedback to the player.
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+# Include two different difficulty levels (e.g., 10 guesses in easy mode,\
+#  only 5 guesses in hard mode).
 
 import random
 import art
@@ -48,7 +50,7 @@ def game_play(attempts_available):
             break
 
         attempts_available -= 1
-        if (attempts_available <= 0):
+        if attempts_available <= 0:
             print(f"The number to be guessed was {NUMBER}")
             print("You have run out of guesses, you lose")
             break
